3c572f5d25a22350c8b5c799761afb3055d6570c
Kevin Ha <kcha@users.noreply.github.com>
2018-10-04 19:49:51 -0400

    Read dataframe in chunks to save memory

--- pyconserve/summarize.py
+++ pyconserve/summarize.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import pandas as pd
+import multiprocessing
 
 from . import reduce_df
 from .version import __version__
@@ -10,13 +11,18 @@ def getoptions():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('bedfile', metavar='BED', nargs=1,
                         help="Input BED file")
+    parser.add_argument('-c', '--cores', type=int, default=4,
+                        help="Number of processing cores [%(default)s]")
+    parser.add_argument('-b', '--batchsize', type=int,
+                        default=2000000,
+                        help="Chunk size [%(default)s]")
     parser.add_argument('-v', '--version', action='version',
         version='%(prog)s ' + __version__)
     args = parser.parse_args()
     return args
 
 
-def load_intersect(filename, **kwargs):
+def load_intersect(filename, chunksize=2000000, **kwargs):
     """
     Load intersected BED file as DataFrame
     """
@@ -35,29 +41,46 @@ def load_intersect(filename, **kwargs):
         'end2': 'uint32'
     }
 
-    df = pd.read_table(filename, names=colnames, 
-                       usecols=usecolnames,
-                       dtype=column_types)
-    #return reduce_df.reduce_mem_usage(df, **kwargs)[0]
-    return df
+    reader = pd.read_table(filename, names=colnames, 
+                               usecols=usecolnames,
+                               dtype=column_types,
+                               chunksize=chunksize)
+    return reader
    
 
-def groupby_cons(df):
-    """
-    Group by BED interval and report the average score
-    """
+def sum_scores(data):
+    le = data.end2 - data.start2
+    N = sum(le)
+    total_score = sum(data.score2 * le)
+    return pd.DataFrame({'N': [N], 'total_score': [total_score]})
+
+
+def count_scores(df):
     result = df.groupby('name')\
-               .apply(compute_average_cons)\
-               .reset_index(name="avg_cons")
+               .apply(sum_scores)
     return result
+
+
+# def groupby_cons(df):
+#     """
+#     Group by BED interval and report the average score
+#     """
+#     result = df.groupby('name')\
+#                .apply(compute_average_cons)\
+#                .reset_index(name="avg_cons")
+#     return result
 
 
 def compute_average_cons(data):
     """
     Compute average conservation 
     """
-    le = data.end2 - data.start2
-    return sum(data.score2 * le) / sum(le)
+    #le = data.end2 - data.start2
+    #N = sum(le)
+    #return sum(data.score2 * le) / N
+    data = data.groupby('name').agg('sum')
+    data['avg_cons'] = data.total_score / data.N
+    return data
 
 
 def main():
@@ -68,14 +91,17 @@ def main():
     args = getoptions()
 
     if args.bedfile[0] == '-':
-        df = load_intersect(sys.stdin)
+        reader = load_intersect(sys.stdin, args.batchsize)
     else:
-        df = load_intersect(args.bedfile[0])
+        reader = load_intersect(args.bedfile[0], args.batchsize)
 
-    df = groupby_cons(df)
-    #df = df.drop(['start2', 'end2'])
+    pool = multiprocessing.Pool(args.cores)
+    df = pd.concat(pool.map(count_scores, reader))
+    pool.close()
 
-    df.to_csv(sys.stdout, sep="\t", index=False, header=False)
+    df = compute_average_cons(df)
+
+    df.loc[:,'avg_cons'].to_csv(sys.stdout, sep="\t", index=True, header=False)
 
 
 if __name__ == '__main__':
